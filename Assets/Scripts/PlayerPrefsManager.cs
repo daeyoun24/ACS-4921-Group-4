7898b1b6920723ac5b7d177416f1d060810ebd45
daeyoun24 <daeyoun1506@gmail.com>
2015-10-19 10:47:55 -0500

    Add AESEncryption
    Also changed a setting: Player settings -> Other settings -> API... ->
    .Net 2.0 (to use AESProvider)

--- Assets/Scripts/PlayerPrefsManager.cs
+++ Assets/Scripts/PlayerPrefsManager.cs
@@ -4,18 +4,21 @@ using System.Collections;
 public class PlayerPrefsManager : MonoBehaviour
 {
     private const string UNLOCK_KEY = "game_unlocked";
-    private const string UNLOCK_SECURED_KEY = "game_unlocked_test";
+    private const string UNLOCK_DES_KEY = "game_unlocked_DES";
+    private const string UNLOCK_AES_KEY = "game_unlocked_AES";
 
     public static void UnlockGame()
     {
         PlayerPrefs.SetString(UNLOCK_KEY, "unlocked");
-        SecurePlayerPrefs.SetString(UNLOCK_SECURED_KEY, "unlocked", "mykey");
+        SecurePlayerPrefs.SetString(UNLOCK_DES_KEY, "unlocked", "mykey");
+        SecurePlayerPrefsAES.SetString(UNLOCK_AES_KEY, "unlocked", "mykey");
     }
 
     public static void LockGame()
     {
         PlayerPrefs.SetString(UNLOCK_KEY, "locked");
-        SecurePlayerPrefs.SetString(UNLOCK_SECURED_KEY, "locked", "mykey");
+        SecurePlayerPrefs.SetString(UNLOCK_DES_KEY, "locked", "mykey");
+        SecurePlayerPrefsAES.SetString(UNLOCK_AES_KEY, "locked", "mykey");
     }
 
     public static bool IsGameUnlocked()
@@ -29,7 +32,7 @@ public class PlayerPrefsManager : MonoBehaviour
         //    return false;
         //}
 
-        if (SecurePlayerPrefs.GetString(UNLOCK_SECURED_KEY, "mykey") == "unlocked")
+        if (SecurePlayerPrefs.GetString(UNLOCK_DES_KEY, "mykey") == "unlocked")
         {
             return true;
         }
